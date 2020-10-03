47c4bd4e27d4daa9c4213d9da50be3ca2adeb301
Pierre-Luc Bruyere <proteama@yahoo.com>
2020-10-03 14:57:21 -0400

    Spawning Damage Text

---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Spawn(11);
+        }
+
+        public void Spawn(float damageAmount)
+        {
+            if (damageTextPrefab)
+            {
+                DamageText instance = GameObject.Instantiate(damageTextPrefab, transform);
+            }
+        }
+    }
+}
