04a3bde75de1ff58a5130bf0989391bf7cb00889
Pierre-Luc Bruyere <proteama@yahoo.com>
2020-10-06 16:06:27 -0400

    Fixing the Fader

--- Assets/Scripts/Scene Management/Fader.cs
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -8,41 +8,49 @@ namespace RPG.SceneManagement
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
+        Coroutine currentActiveFade = null;
 
         void Awake()
         {
             canvasGroup = GetComponent<CanvasGroup>();
         }
 
-        // Nesting co-routines
-/*
-        IEnumerator FadeOutIn()
+        public IEnumerator Fade(float target, float fadeTime)
         {
-            yield return FadeOut(fadeTime);
-            yield return FadeIn(fadeTime);
+            if (currentActiveFade != null)
+            {
+                StopCoroutine(currentActiveFade);
+            }
+            currentActiveFade = StartCoroutine(FadeRoutine(target, fadeTime));
+            yield return currentActiveFade;
         }
-*/
+
         public IEnumerator FadeIn(float fadeTime)
         {
-            while (canvasGroup.alpha > 0.0f)
-            {
-                canvasGroup.alpha = Mathf.Clamp01(canvasGroup.alpha - Time.deltaTime / fadeTime);
-                yield return null;
-            }
+            return Fade(0.0f, fadeTime);
         }
 
         public IEnumerator FadeOut(float fadeTime)
         {
-            while (canvasGroup.alpha < 1.0f)
-            {
-                canvasGroup.alpha = Mathf.Clamp01(canvasGroup.alpha + Time.deltaTime / fadeTime);
-                yield return null;
-            }
+            return Fade(1.0f, fadeTime);
         }
 
         public void FadeOutImmediate()
         {
+            if (currentActiveFade != null)
+            {
+                StopCoroutine(currentActiveFade);
+            }
             canvasGroup.alpha = 1.0f;
+        }
+
+        IEnumerator FadeRoutine(float target, float fadeTime)
+        {
+            while (!Mathf.Approximately(canvasGroup.alpha, target))
+            {
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / fadeTime);
+                yield return null;
+            }
         }
     }
 }
